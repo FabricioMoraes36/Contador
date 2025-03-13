427b37a5f28518b4da1a9043137a19756b5a5645
Fabricio Moraes <Fabricio36365@outlook.com>
2025-03-13 16:08:34 -0300

    Codigo usando try catch para uma exceção personalizada e tratando

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor: ");
+        int numero1 = leitura.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        int numero2 = leitura.nextInt();
+        try{
+            if(numero1 > numero2){
+throw new ParametrosInvalidosException();
+            }
+            for(int contador = numero1; contador <= numero2; contador++) {
+                System.out.println("o intervalo entre os numeros é: " + contador);
+            }
+        }catch (ParametrosInvalidosException e){
+            System.out.println("ta errado o valor 1");
+        }
+    }
+}
